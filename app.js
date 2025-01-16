39fecffd273d3b17d8c3b115b1a0eed9688477d4
Saunders <ssaunders@una.edu>
2025-01-16 16:32:30 -0600

    Threw it on the server and added it to the console

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000;
 
-//endpoint, middlewares(s)
+// endpoint, middlewares(s)
 app.get('/', function (req, res) {
     res.send('Hello Express')
 })
 
-app.listen(3000)
+app.listen(port, ()=> console.log(`server is running on ... ${port}`));
+
+// when running run "npm i express" prior to running the code
