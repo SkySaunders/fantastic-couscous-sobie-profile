c600dd19bb6f3d07eec049075750b52ad91cdb2a
Saunders <ssaunders@una.edu>
2025-01-21 16:25:47 -0600

    Able to go back and forth

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'))
+
 // endpoint, middlewares(s)
 app.get('/', function (req, res) {
-    res.send('Hello Express')
+    res.sendFile('index.html')
 })
 
+app.get('/helloRender', function (req, res){
+    res.send('Hello from Real World<br><a href="/">return home<a>')
+})
 app.listen(port, ()=> console.log(`server is running on ... ${port}`));
 
 // when running run "npm i express" prior to running the code
