ccf60427f03cbfefcebc7ff2d4490e2da1562c04
Saunders <ssaunders@una.edu>
2025-01-23 16:32:01 -0600

    added a whole lot. used GET and POST.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'))
 
 // endpoint, middlewares(s)
@@ -16,12 +18,29 @@ app.get('/nodemon', function (req, res) {
 })
 
 app.get('/ejs', function (req, res) {
-    res.render('words')
+    res.render('words', {PageTitle: req.body.MyName});
 })
 
-app.get('/helloRender', function (req, res){
+app.get('/helloRender', function (req, res) {
     res.send('Hello from Real World<br><a href="/">return home<a>')
 })
-app.listen(port, ()=> console.log(`server is running on ... ${port}`));
+//uses POST method to get info from the dom
+app.post('/saveMyName', function (req, res) {
+    console.log('did we hit the endpoint?');
+    console.log(req.body);
+    res.redirect('/ejs');
+})
+//uses query from dom to get information
+app.get('/saveMyNameGet', function (req, res) {
+    console.log('did we hit the get endpoint?');
+    console.log(req.query);
+    res.redirect('/ejs');
+})
 
-// when running run "npm i express" prior to running the code
+app.listen(port, () => console.log(`server is running on ... ${port}`));
+
+/*  
+"npm i express"
+"npm i body-parser"
+"npm i ejs"
+*/
